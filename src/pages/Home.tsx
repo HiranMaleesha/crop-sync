a1edb32e361ff85a296f4764aed3505568ce04d8
HiranMaleesha <maleesha20010527@gmail.com>
2025-03-10 00:13:45 +0530

    Initial commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className="dashboard">
+      <div className="dashboard-header">
+        <h2>Dashboard</h2>
+        <div className="search-container">
+         
+          <input type="text" placeholder="Search..." className="search-input" />
+        </div>
+      </div>
+      <div className="dashboard-grid">
+        
+        
+      </div>
+    </div>
+  );
+};
+
+export default Home
