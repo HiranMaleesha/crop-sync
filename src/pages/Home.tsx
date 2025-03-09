a01e46069e3b46f8d8ba5803e0a32d1542ad5a60
HiranMaleesha <maleesha20010527@gmail.com>
2025-03-10 00:40:05 +0530

    hi

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,19 +2,14 @@ import React from 'react'
 
 function Home() {
   return (
-    <div className="dashboard">
-      <div className="dashboard-header">
-        <h2>Dashboard</h2>
-        <div className="search-container">
-         
-          <input type="text" placeholder="Search..." className="search-input" />
-        </div>
-      </div>
-      <div className="dashboard-grid">
-        
-        
-      </div>
+    <div>
+      <h1>
+        Hi my babyy gurlll
+        I love youuu
+      </h1>
+
     </div>
+    
   );
 };
 
